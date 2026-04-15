f1fd7d13f9b277a3cb82059be23529d36a462b0e
osakenpiro <kenpikenpi64@gmail.com>
2026-04-15 18:40:35 +0900

    v0.1: vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+export default defineConfig({
+  plugins: [react()],
+  base: '/tana-zukan/',
+  build: { outDir: 'docs', emptyOutDir: true }
+})
